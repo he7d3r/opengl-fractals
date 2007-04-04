3e2269624c4e71c123294c694b41ab392d100c64
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2007-04-04 13:07:00 -0300

    Test point at the infinity

---
+++ pirâmide/TESTES~1_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef TESTES~1_PRIVATE_H
+#define TESTES~1_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.1.2.924"
+#define VER_MAJOR	0
+#define VER_MINOR	1
+#define VER_RELEASE	2
+#define VER_BUILD	924
+#define COMPANY_NAME	""
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"Desenvolvido por Helder Geovane Gomes de Lima, utilizando Dev-C++ IDE"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copie logo, enquanto não foi registrado!"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	""
+
+#endif /*TESTES~1_PRIVATE_H*/
